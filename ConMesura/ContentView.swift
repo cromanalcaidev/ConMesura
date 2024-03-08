4e98df398508f560e86d550f3ede60b1e189152d
Carlos Román Alcaide <cromanalcaidev@gmail.com>
2024-03-09 00:11:17 +0100

    First commit.
    Messed up the 1st project so I had to rebuild it from the ground up.
    
    Created the pickers, textfields and general structure.

--- ConMesura/ContentView.swift
+++ ConMesura/ContentView.swift
@@ -2,20 +2,70 @@
 //  ContentView.swift
 //  ConMesura
 //
-//  Created by Carlos Román Alcaide on 9/3/24.
+//  Created by Carlos Román Alcaide on 8/3/24.
 //
+
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var input = 0.0
+    @State private var placeholderInputUnit = "C"
+    @State private var placeholderOutputUnit = "K"
+    @State private var outputUnit = 0.0
+    let inputUnits = ["K", "F", "C"]
+    let outputUnits = ["K", "F", "C"]
+    
+    var returnUnit: String {
+        
+        
+//
+//        switch inputUnits {
+//        case ["K"]:
+//            outputUnit = input * 1.3
+//            print(outputUnit)
+//        case ["F"]:
+//            print("yay")
+//        case ["C"]:
+//            print("ajua")
+//        default:
+//            print("nope")
+//        }
+//
+//
+//        switch outputUnits {
+//        case ["F"]:
+//            outputUnit *= 1.3
+//        default:
+//            print("nope")
+//        }
+        
+        return "Keep trying boyo"
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Unit", value: $input, format: .number)
+                        .keyboardType(.decimalPad)
+                    HStack {
+                        Picker("Select Input Unit", selection: $placeholderInputUnit) {
+                            ForEach(inputUnits, id: \.self) {
+                            Text("\($0)")}
+                        }
+                        Picker("Select Output Unit", selection: $placeholderOutputUnit) {
+                            ForEach(outputUnits, id: \.self) {
+                            Text("\($0)")}
+                        }
+                    }
+                    Section {
+                        Text(returnUnit)
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
