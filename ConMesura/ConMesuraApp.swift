4fb7005e6a106a40f089b1eeb5b50eae1bec3aed
Carlos Román Alcaide <cromanalcaidev@gmail.com>
2024-03-09 00:09:14 +0100

    Initial Commit

---
+++ ConMesura/ConMesuraApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConMesuraApp.swift
+//  ConMesura
+//
+//  Created by Carlos Román Alcaide on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ConMesuraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConMesura/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ConMesura
+//
+//  Created by Carlos Román Alcaide on 9/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
